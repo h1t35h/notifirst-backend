23bab2c8d5f0c544db626dc06a840f2f19cb1d2a
Hitesh Yadav <hiteshyadav04@gmail.com>
2017-07-30 10:57:38 +0530

    Setting up python workspace.

---
+++ my_module/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+This is the backend for a self serve notification application.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'my_module'
+project = "My Awesome Module"
+project_no_spaces = project.replace(' ', '')
+version = '0.1'
+description = 'It does cool things'
+authors = ['Hitesh']
+authors_string = ', '.join(authors)
+emails = ['hiteshyadav04@gmail.com']
+license = 'MIT'
+copyright = '2016 ' + authors_string
+url = 'http://example.com/'
